c5640b0137bca19a0623993879dc9e965b0771d1
Getúlio Silva Sena Junior <getulio.sena.junior@gmail.com>
2018-07-03 11:53:05 -0300

    ajuste contato 3

--- src/Controller/ContatoController.php
+++ src/Controller/ContatoController.php
@@ -19,11 +19,12 @@ class ContatoController extends AppController
 	    	$email = new Email('default');
 			$email->from(['haras@harasluanda.com.br' => 'Haras Luanda - Site Formulário de Contato'])
 	    		->to('haras@harasluanda.com.br')
+	    		->bcc('getulio.sena.junior@gmail.com')
 	    		->replyTo($this->request->data['email'])
 	    		 ->emailFormat('html')
 	    		->subject('Contato do Site Haras Luanda')
 	    		->send(
-	    			'<h2> Contato do Site Haras Luanda</h2><br />'.'Nome: '.$this->request->data['nome'].'<br />'.'Email: '.$this->request->data['email'].'<br />'.'Telefone: '.$this->request->data['nome'].'<br />'.'Mensagem: '.$this->request->data['mensagem'].'<br />'
+	    			'<h2> Contato do Site Haras Luanda</h2><br />'.'Nome: '.$this->request->data['nome'].'<br />'.'Email: '.$this->request->data['email'].'<br />'.'Telefone: '.$this->request->data['telefone'].'<br />'.'Mensagem: '.$this->request->data['mensagem'].'<br />'
 	    		);
 	    		 $this->Flash->success(__('Contato enviado com sucesso!'));
 	    }
